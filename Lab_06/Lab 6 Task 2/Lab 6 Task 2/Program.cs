a0d525d33df173ed0e0aed49a0e45062f2a0eb03
muaazshoaib <muaazshoaib1670@gmail.com>
2023-10-25 09:47:40 +0500

    ...

---
+++ Lab_06/Lab 6 Task 2/Lab 6 Task 2/Program.cs
@@ -0,0 +1,97 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+class GrammarAnalyzer
+{
+    static Dictionary<string, List<string>> grammarRules = new Dictionary<string, List<string>>();
+    static Dictionary<string, HashSet<string>> firstSets = new Dictionary<string, HashSet<string>>();
+
+    static void Main()
+    {
+        Console.WriteLine("Enter the grammar rules (one rule per line, non-terminal followed by '->' and its productions separated by '|'):");
+        Console.WriteLine("Example: S -> aABc | BC");
+        Console.WriteLine("Enter 'q' to quit.");
+
+        while (true)
+        {
+            Console.Write("Enter a rule (or 'q' to quit): ");
+            string rule = Console.ReadLine().Trim();
+
+            if (rule.ToLower() == "q")
+                break;
+
+            ParseGrammarRule(rule);
+        }
+
+        CalculateFirstSets();
+
+        // Print the FIRST sets
+        Console.WriteLine("\nFIRST Sets:");
+        foreach (var nonTerminal in firstSets.Keys)
+        {
+            Console.WriteLine($"{nonTerminal}: {{{string.Join(", ", firstSets[nonTerminal])}}}");
+        }
+    }
+
+    static void ParseGrammarRule(string rule)
+    {
+        string[] parts = rule.Split(new[] { "->" }, StringSplitOptions.RemoveEmptyEntries);
+
+        if (parts.Length != 2)
+        {
+            Console.WriteLine("Invalid input. Please enter a valid grammar rule.");
+            return;
+        }
+
+        string nonTerminal = parts[0].Trim();
+        string[] productions = parts[1].Split('|').Select(p => p.Trim()).ToArray();
+
+        if (!grammarRules.ContainsKey(nonTerminal))
+        {
+            grammarRules[nonTerminal] = new List<string>();
+        }
+
+        grammarRules[nonTerminal].AddRange(productions);
+    }
+
+    static void CalculateFirstSets()
+    {
+        foreach (var nonTerminal in grammarRules.Keys)
+        {
+            CalculateFirstSet(nonTerminal);
+        }
+    }
+
+    static void CalculateFirstSet(string nonTerminal)
+    {
+        if (!firstSets.ContainsKey(nonTerminal))
+            firstSets[nonTerminal] = new HashSet<string>();
+
+        foreach (var production in grammarRules[nonTerminal])
+        {
+            CalculateFirstSetForProduction(nonTerminal, production);
+        }
+    }
+
+    static void CalculateFirstSetForProduction(string nonTerminal, string production)
+    {
+        if (string.IsNullOrEmpty(production))
+        {
+            // If the production is empty, add epsilon to FIRST(nonTerminal)
+            firstSets[nonTerminal].Add("");
+        }
+        else if (char.IsUpper(production[0]))
+        {
+            // First symbol is a non-terminal
+            string firstSymbol = production[0].ToString();
+            CalculateFirstSet(firstSymbol);
+            firstSets[nonTerminal].UnionWith(firstSets[firstSymbol]);
+        }
+        else
+        {
+            // First symbol is a terminal
+            firstSets[nonTerminal].Add(production[0].ToString());
+        }
+    }
+}
---
+++ Lab_06/Lab 6 Task/Lab 6 Task/Program.cs
@@ -0,0 +1,107 @@
+﻿using System;
+using System.Collections.Generic;
+
+class GrammarParser
+{
+    static void Main()
+    {
+        // Define grammar rules as a dictionary of non-terminals and their production rules
+        Dictionary<string, List<string>> grammarRules = new Dictionary<string, List<string>>
+        {
+            { "S", new List<string> { "AB", "BC" } },
+            { "A", new List<string> { "a", "ε" } },
+            { "B", new List<string> { "b", "ε" } },
+            { "C", new List<string> { "c" } },
+        };
+
+        // Calculate and print the FIRST sets
+        Dictionary<string, HashSet<string>> firstSets = CalculateFirstSets(grammarRules);
+        foreach (var nonTerminal in firstSets.Keys)
+        {
+            Console.WriteLine($"FIRST({nonTerminal}) = {{{string.Join(", ", firstSets[nonTerminal])}}}");
+        }
+    }
+
+    static Dictionary<string, HashSet<string>> CalculateFirstSets(Dictionary<string, List<string>> grammarRules)
+    {
+        var firstSets = new Dictionary<string, HashSet<string>>();
+
+        foreach (var nonTerminal in grammarRules.Keys)
+        {
+            firstSets[nonTerminal] = new HashSet<string>();
+        }
+
+        bool changed;
+        do
+        {
+            changed = false;
+
+            foreach (var nonTerminal in grammarRules.Keys)
+            {
+                foreach (var production in grammarRules[nonTerminal])
+                {
+                    foreach (var symbol in production)
+                    {
+                        if (IsTerminal(symbol.ToString()))
+                        {
+                            if (firstSets[nonTerminal].Add(symbol.ToString()))
+                            {
+                                changed = true;
+                            }
+                            break;
+                        }
+                        else if (IsNonTerminal(symbol.ToString()))
+                        {
+                            bool hasEpsilon = true;
+                            foreach (var s in grammarRules[symbol.ToString()])
+                            {
+                                foreach (var subSymbol in s)
+                                {
+                                    if (!firstSets[symbol.ToString()].Add(subSymbol.ToString()))
+                                    {
+                                        break;
+                                    }
+                                    if (!IsEpsilon(subSymbol.ToString()))
+                                    {
+                                        hasEpsilon = false;
+                                        break;
+                                    }
+                                }
+                                if (!hasEpsilon)
+                                {
+                                    break;
+                                }
+                            }
+                            if (hasEpsilon)
+                            {
+                                if (firstSets[nonTerminal].Add("ε"))
+                                {
+                                    changed = true;
+                                }
+                            }
+                        }
+                    }
+                }
+            }
+        } while (changed);
+
+        return firstSets;
+    }
+
+    static bool IsTerminal(string symbol)
+    {
+        return char.IsLower(symbol[0]) || symbol == "ε";
+    }
+
+    static bool IsNonTerminal(string symbol)
+    {
+        return char.IsUpper(symbol[0]);
+    }
+
+    static bool IsEpsilon(string symbol)
+    {
+        return symbol == "ε";
+    }
+}
+
+
